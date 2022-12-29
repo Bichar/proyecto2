af3cb8bb4b9bb77b7872d72a2797e1251587c291
Bichar <rogeliochavez949@gmail.com>
2022-12-29 09:03:04 -0600

    1

---
+++ src/com/mycompany/proyecto/Conexion.java
@@ -0,0 +1,33 @@
+
+package com.mycompany.proyecto;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ *
+ *
+ */
+public class Conexion {
+  
+
+    public Connection getConexion(){
+         try{   
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/dbparadigmas","root","lpooMy5@l");
+            return conn;
+        
+        }catch(ClassNotFoundException clex){
+            System.out.print(clex);
+    }   catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      return null;
+    }
+}
